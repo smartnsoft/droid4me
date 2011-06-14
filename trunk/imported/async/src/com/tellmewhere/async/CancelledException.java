4d1c55316b09264718869ae3be1d5cd6232ff223
edouard@smartnsoft.com <edouard@smartnsoft.com@5b56cd44-6705-69f8-8314-8c0163dbad92>
2011-06-14 22:00:44 +0000

    An import error.

--- trunk/imported/async/src/com/tellmewhere/async/CancelledException.java
+++
@@ -1,6 +0,0 @@
-package com.tellmewhere.async;
-
-public class CancelledException extends Exception
-{
-	private static final long serialVersionUID = 1L;
-}
--- trunk/imported/async/src/com/tellmewhere/async/DeferredCallable.java
+++
@@ -1,7 +0,0 @@
-package com.tellmewhere.async;
-
-public interface DeferredCallable
-{
-	public Object onSuccess(Object param) throws Exception;
-	public Object onFailure(Exception e) throws Exception;
-}
