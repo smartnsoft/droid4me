1583ddfcc8a73155924f672bd8f102b1565c1ce9
edouard@smartnsoft.com <edouard@smartnsoft.com@5b56cd44-6705-69f8-8314-8c0163dbad92>
2011-08-17 14:27:35 +0000

    Enables to better tune the logging.

--- trunk/src/com/smartnsoft/droid4me/log/LoggerFactory.java
+++ trunk/src/com/smartnsoft/droid4me/log/LoggerFactory.java
@@ -24,6 +24,11 @@ import android.util.Log;
  * In order to have an entry point for the logging interface. Because, when we use the Android logger, there are problems during the unitary tests on
  * a desktop machine.
  * 
+ * <p>
+ * When the the Java system property <code>droid4me.logging</code> is defined with the value "false", the logging uses the standard error and output
+ * streams. This is useful when unit-testing the framework.
+ * </p>
+ * 
  * @author Édouard Mercier
  * @date 2008.01.15
  */
@@ -32,10 +37,112 @@ public class LoggerFactory
 
   public static int logLevel = Log.WARN;
 
+  private static Boolean enabled;
+
+  /**
+   * An implementation which uses the Java standard output and error streams.
+   */
+  private final static class NullLogger
+      implements Logger
+  {
+
+    public boolean isDebugEnabled()
+    {
+      return LoggerFactory.logLevel <= Log.DEBUG;
+    }
+
+    public boolean isInfoEnabled()
+    {
+      return LoggerFactory.logLevel <= Log.INFO;
+    }
+
+    public boolean isWarnEnabled()
+    {
+      return LoggerFactory.logLevel <= Log.WARN;
+    }
+
+    public boolean isErrorEnabled()
+    {
+      return LoggerFactory.logLevel <= Log.ERROR;
+    }
+
+    public boolean isFatalEnabled()
+    {
+      return LoggerFactory.logLevel <= Log.ERROR;
+    }
+
+    public void debug(String message)
+    {
+      System.out.println(message);
+    }
+
+    public void error(String message)
+    {
+      System.err.println(message);
+    }
+
+    public void error(String message, Throwable throwable)
+    {
+      System.err.println(message);
+      throwable.printStackTrace(System.err);
+    }
+
+    public void error(StringBuffer message, Throwable throwable)
+    {
+      System.err.println(message);
+      throwable.printStackTrace(System.err);
+    }
+
+    public void fatal(String message)
+    {
+      System.err.println(message);
+    }
+
+    public void fatal(String message, Throwable throwable)
+    {
+      System.err.println(message);
+      throwable.printStackTrace(System.err);
+    }
+
+    public void info(String message)
+    {
+      System.out.println(message);
+    }
+
+    public void warn(String message)
+    {
+      System.out.println(message);
+    }
+
+    public void warn(String message, Throwable throwable)
+    {
+      System.out.println(message);
+      throwable.printStackTrace(System.out);
+    }
+
+    public void warn(StringBuffer message, Throwable throwable)
+    {
+      System.out.println(message);
+      throwable.printStackTrace(System.out);
+    }
+
+  }
+
   @SuppressWarnings("unchecked")
   public static Logger getInstance(Class theClass)
   {
-    return new AndroidLogger(theClass);
+    if (LoggerFactory.enabled == null)
+    {
+      LoggerFactory.enabled = System.getProperty("droid4me.logging", "true").equals("false") == false;
+    }
+    if (LoggerFactory.enabled == true)
+    {
+      return new AndroidLogger(theClass);
+    }
+    else
+    {
+      return new NullLogger();
+    }
   }
 
 }
