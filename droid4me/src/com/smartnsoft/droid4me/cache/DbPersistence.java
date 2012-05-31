4122b70656da17ceae41a9c536c680d816dea886
edouard@smartnsoft.com <edouard@smartnsoft.com>
2012-05-31 09:27:04 +0000

    Fixes a missing and mean database cursor release bug, during the "LastUpdateDbCleanUpPolicy.cleanUp()" method.

--- droid4me/src/com/smartnsoft/droid4me/cache/DbPersistence.java
+++ droid4me/src/com/smartnsoft/droid4me/cache/DbPersistence.java
@@ -135,6 +135,7 @@ public final class DbPersistence
         {
           writeableDatabase.setTransactionSuccessful();
           writeableDatabase.endTransaction();
+          cursor.close();
         }
       }
     }
