70a2484971cfb6f88b3433d4f967b1ae97758f6d
edouard@smartnsoft.com <edouard@smartnsoft.com@5b56cd44-6705-69f8-8314-8c0163dbad92>
2012-12-25 19:39:37 +0000

    Adds the "SmartPageIndicator" component.

---
+++ trunk/droid4me/src/com/smartnsoft/droid4me/widget/SmartPageIndicator.java
@@ -0,0 +1,68 @@
+package com.smartnsoft.droid4me.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+/**
+ * A page view component.
+ * 
+ * @since 2011.01.10
+ * @author ɉdouard Mercier
+ */
+public class SmartPageIndicator
+    extends LinearLayout
+{
+
+  public SmartPageIndicator(Context context, AttributeSet attrs)
+  {
+    super(context, attrs);
+    setOrientation(LinearLayout.HORIZONTAL);
+  }
+
+  public SmartPageIndicator(Context context)
+  {
+    super(context);
+    setOrientation(LinearLayout.HORIZONTAL);
+  }
+
+  /**
+   * Indicates how many panes the pager is made of, and the way the pager dots should look like.
+   * 
+   * @param count
+   *          the number of pages
+   * @param dotImageResourceId
+   *          the drawable resources which should be used to draw the dots ; it is important that this drawable has selected and un-selected stats
+   * @param dotSpacing
+   *          the spacing between two dots
+   */
+  public void setPagesCount(int count, int dotImageResourceId, int dotSpacing)
+  {
+    if (count == getChildCount())
+    {
+      return;
+    }
+    removeAllViews();
+    final Context context = getContext();
+    for (int index = 0; index < count; index++)
+    {
+      final ImageView imageView = new ImageView(context);
+      imageView.setSelected(false);
+      imageView.setImageResource(dotImageResourceId);
+      final LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+      layoutParams.leftMargin = dotSpacing / 2;
+      layoutParams.rightMargin = dotSpacing / 2;
+      addView(imageView, layoutParams);
+    }
+  }
+
+  public void setSelection(int position)
+  {
+    for (int index = 0; index < getChildCount(); index++)
+    {
+      getChildAt(index).setSelected(index == position);
+    }
+  }
+
+}
