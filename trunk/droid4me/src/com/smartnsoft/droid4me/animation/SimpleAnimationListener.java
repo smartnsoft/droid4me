10c5258429e4baba5724b5935c384b954c1e104f
edouard@smartnsoft.com <edouard@smartnsoft.com@5b56cd44-6705-69f8-8314-8c0163dbad92>
2013-03-26 19:27:25 +0000

    Adds a handy "Animation.AnimationListener" abstract implementation.

---
+++ trunk/droid4me/src/com/smartnsoft/droid4me/animation/SimpleAnimationListener.java
@@ -0,0 +1,36 @@
+package com.smartnsoft.droid4me.animation;
+
+import android.view.animation.Animation;
+
+/**
+ * An abstract {@link Animation.AnimationListener} which makes possible to override only one method.
+ * 
+ * @author Édouard Mercier
+ * @since 2013.03.26
+ */
+public abstract class SimpleAnimationListener
+    implements Animation.AnimationListener
+{
+
+  /**
+   * Does nothing.
+   */
+  public void onAnimationEnd(Animation animation)
+  {
+  }
+
+  /**
+   * Does nothing.
+   */
+  public void onAnimationRepeat(Animation animation)
+  {
+  }
+
+  /**
+   * Does nothing.
+   */
+  public void onAnimationStart(Animation animation)
+  {
+  }
+
+}
