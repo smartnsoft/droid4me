98c0cbd87a4fe09449e65b3f28b086c97fedb3e8
Yannick Platon <yannick@smartnsoft.com>
2017-02-23 18:17:23 +0100

    RED-149 Remove the previous update on the SmartAppCompatActivity

--- library/src/main/java/com/smartnsoft/droid4me/support/v7/app/SmartAppCompatActivity.java
+++ library/src/main/java/com/smartnsoft/droid4me/support/v7/app/SmartAppCompatActivity.java
@@ -32,7 +32,6 @@ import android.view.MenuItem;
 import com.smartnsoft.droid4me.app.AppPublics.BroadcastListener;
 import com.smartnsoft.droid4me.app.Droid4mizer;
 import com.smartnsoft.droid4me.app.SmartableActivity;
-import com.smartnsoft.droid4me.support.v4.app.SmartFragmentActivity;
 
 /**
  * @author Jocelyn Girard
